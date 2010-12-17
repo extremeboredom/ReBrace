ca4f5ee19c91ceaf0b61f1c477b3dc76334d5b26
Adam Cox <extreme.boredom@gmail.com>
2010-12-17 17:17:53 +0000

    Added basic test case

---
+++ test_rebrace.rb
@@ -0,0 +1,25 @@
+require 'rebrace.rb'
+require 'test/unit'
+
+class TestRebracer < Test::Unit::TestCase
+  def rebraceToString(input)
+    output = ""
+    Rebracer.new(input).rebrace { |line| output += line }
+    return output
+  end
+  
+  def test_simple
+    input = <<-END_OF_CODE
+      if (x == 1) {
+        ++x;
+      }
+    END_OF_CODE
+    expected = <<-END_OF_CODE
+      if (x == 1) 
+      {
+        ++x;
+      }
+    END_OF_CODE
+    assert_equal(expected, rebraceToString(input))
+  end
+end
